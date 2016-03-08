cb848fc69f4052e458cdbc2a4ccc6732199b5d37
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-03-08 15:05:42 +0100

    set sep char & adjust row indices; rename file

--- varcontext/create_context.pl
+++ varcontext/create_context.pl
@@ -14,16 +14,16 @@ use ensembl;
 
 my $vs = VariantSet->new(canonical=>1, fullpeptide=>1);
 
-my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
+my $csv = Text::CSV->new ( { 'binary' => 1, 'sep_char' => "\t" } )  # should set binary attribute.
 	or die "Cannot use CSV: ".Text::CSV->error_diag ();
 my $file = shift;
 
 open my $fh, "<:encoding(utf8)", $file or die "$file: $!";
 $csv->column_names ($csv->getline ($fh));
 while ( my $row = $csv->getline( $fh ) ) {
-	(my $ref = $row->[5]) =~ s/-//g;
-	(my $alt = $row->[6]) =~ s/-//g;
-	my $v = Variant->new(chr=>$row->[2], start=>$row->[3], end=>$row->[4], ref=>$ref, alt=>$alt);
+	(my $ref = $row->[9]) =~ s/-//g;
+	(my $alt = $row->[10]) =~ s/-//g;
+	my $v = Variant->new(id=>$row->[1], chr=>$row->[5], start=>$row->[6], end=>$row->[7], ref=>$ref, alt=>$alt);
 	$vs->add($v);
 }
 $csv->eof or $csv->error_diag();
@@ -32,6 +32,3 @@ close $fh;
 $vs->group_variants;
 $vs->apply_variants;
 $vs->print_variant_context;
-
-
-
