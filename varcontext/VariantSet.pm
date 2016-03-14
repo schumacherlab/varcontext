2ac798a45398301c3eca70027f77f865bb8408a3
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-03-14 11:42:16 +0100

    fix bool operator

--- varcontext/VariantSet.pm
+++ varcontext/VariantSet.pm
@@ -183,8 +183,8 @@ sub print_variant_context {
 				$result{aa_alt} = $codontable->translate($result{codon_alt});
 				$v->{type_effect} = $result{aa_ref} eq $result{aa_alt} ? "silent_mutation" : "missense_mutation";
 				
-				if (($result{aa_ref} == "*" && $result{aa_alt} != "*") || ($result{aa_ref} != "*" && $result{aa_alt} == "*")) {
-					$v->{type_effect} = ($result{aa_ref} == "*" && $result{aa_alt} != "*") == 1 ? "stop_lost" : "stop_gained";
+				if (($result{aa_ref} eq "*" && $result{aa_alt} ne "*") || ($result{aa_ref} ne "*" && $result{aa_alt} eq "*")) {
+					$v->{type_effect} = ($result{aa_ref} eq "*" && $result{aa_alt} ne "*") == 1 ? "stop_lost" : "stop_gained";
 				}
 				#check translated codon to substr in pepseq
 				if ($result{aa_ref} ne substr($refpepseq, $refpepstart,1) || $result{aa_alt} ne substr($tumorpepseq, $tumorpepstart,1) ) {
