1ea6cdc976932588bb5d0072f72de2541bf0cc55
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-03-14 11:38:11 +0100

    another typo

--- varcontext/VariantSet.pm
+++ varcontext/VariantSet.pm
@@ -184,7 +184,7 @@ sub print_variant_context {
 				$v->{type_effect} = $result{aa_ref} eq $result{aa_alt} ? "silent_mutation" : "missense_mutation";
 				
 				if (($result{aa_ref} == "*" && $result{aa_alt} != "*") || ($result{aa_ref} != "*" && $result{aa_alt} == "*")) {
-					$v->{type_effect} = ($result{aa_ref} == "*" && result{aa_alt} != "*") == 1 ? "stop_lost" : "stop_gained";
+					$v->{type_effect} = ($result{aa_ref} == "*" && $result{aa_alt} != "*") == 1 ? "stop_lost" : "stop_gained";
 				}
 				#check translated codon to substr in pepseq
 				if ($result{aa_ref} ne substr($refpepseq, $refpepstart,1) || $result{aa_alt} ne substr($tumorpepseq, $tumorpepstart,1) ) {
