74fce14239030584343dcacb3f7efb5fefffc89a
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-05-24 17:13:01 +0200

    match kitchen.pl in master branch

--- varcontext/create_context.pl
+++ varcontext/create_context.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/env perl 
-
 use strict;
 use warnings;
 
+use Data::Dumper;
 use Text::CSV;
+use Getopt::Long;
 
 use FindBin;
 use lib ("$FindBin::Bin","$FindBin::Bin/../editseq" );
@@ -12,18 +12,28 @@ use Variant;
 use VariantSet;
 use ensembl;
 
-my $vs = VariantSet->new(canonical=>0, fullpeptide=>1);
+my $canonical;
+my $fullpeptide;
 
-my $csv = Text::CSV->new ( { 'binary' => 1, 'sep_char' => "\t" } )  # should set binary attribute.
+GetOptions("canonical"=>\$canonical, "fullpeptide"=>\$fullpeptide);
+
+
+my $vs = VariantSet->new(canonical=> $canonical ? 1 : 0, fullpeptide=>$fullpeptide ? 1 : 0);
+
+my $csv = Text::CSV->new ( { binary => 1, sep_char=>"\t" } )  # should set binary attribute.
 	or die "Cannot use CSV: ".Text::CSV->error_diag ();
 my $file = shift;
 
 open my $fh, "<:encoding(utf8)", $file or die "$file: $!";
 $csv->column_names ($csv->getline ($fh));
 while ( my $row = $csv->getline( $fh ) ) {
-	(my $ref = $row->[9]) =~ s/-//g;
-	(my $alt = $row->[10]) =~ s/-//g;
-	my $v = Variant->new(id=>$row->[1], chr=>$row->[5], start=>$row->[6], end=>$row->[7], ref=>$ref, alt=>$alt);
+	(my $ref = $row->[3]) =~ s/-//g;
+	(my $alt = $row->[4]) =~ s/-//g;
+	if($alt =~ m/,/) {
+		warn "Discarding alt for:" . join(",", @$row) . "\n";
+		$alt =~ s/,.*//;
+	}
+	my $v = Variant->new(chr=>$row->[0], start=>$row->[1], ref=>$ref, alt=>$alt);
 	$vs->add($v);
 }
 $csv->eof or $csv->error_diag();
