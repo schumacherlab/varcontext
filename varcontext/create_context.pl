e50bfc627360cdc2824ccad7dbe996e8775eb117
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-06-08 11:59:11 +0200

    don't generate context for SNPs

--- varcontext/create_context.pl
+++ varcontext/create_context.pl
@@ -27,13 +27,17 @@ my $file = shift;
 open my $fh, "<:encoding(utf8)", $file or die "$file: $!";
 $csv->column_names ($csv->getline ($fh));
 while ( my $row = $csv->getline( $fh ) ) {
+	my $id = $row->[2];
 	(my $ref = $row->[3]) =~ s/-//g;
 	(my $alt = $row->[4]) =~ s/-//g;
 	if($alt =~ m/,/) {
 		warn "Discarding alt for:" . join(",", @$row) . "\n";
 		$alt =~ s/,.*//;
+	} else if ($id =~ m/gs/) {
+		warn "Discarding genomic SNP from context generation";
+		next;
 	}
-	my $v = Variant->new(chr=>$row->[0], start=>$row->[1], ref=>$ref, alt=>$alt);
+	my $v = Variant->new(id=>$id, chr=>$row->[0], start=>$row->[1], ref=>$ref, alt=>$alt);
 	$vs->add($v);
 }
 $csv->eof or $csv->error_diag();
