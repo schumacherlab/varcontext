852199a5cc04403122d15f6cd3def2fd31be7545
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-06-23 15:43:19 +0200

    fix env var checking code

--- varcontext/ensembl.pm
+++ varcontext/ensembl.pm
@@ -6,9 +6,12 @@ use Carp;
  
 use FindBin;
 
-croak 'Please set environment variable ENSEMBLAPI to full Ensembl API path' unless defined $ENV{'ENSEMBLAPI'};
+BEGIN { 
+	local $SIG{__DIE__} = sub {warn @_; exit 1};
+	die "Please set environment variable ENSEMBLAPI to full Ensembl API path\n" unless defined $ENV{'ENSEMBLAPI'};
+}
 
-use lib ("$FindBin::Bin", map {$ENV{'ENSEMBLAPI'}. $_} qw(ensembl/modules	ensembl-variation/modules bioperl-live));
+use lib ("$FindBin::Bin", map {$ENV{'ENSEMBLAPI'} . $_} qw(ensembl/modules ensembl-variation/modules bioperl-live));
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::TranscriptMapper;
 use Bio::EnsEMBL::PredictionTranscript;
